5f53656252a8674cf95f303b6e914ee859821b7c
hiwafa <mq.func@gmail.com>
2024-01-10 15:26:14 +0100

    a simple nodejs server with url

--- nodejs-demo002.js
+++ nodejs-demo002.js
@@ -1,6 +1,12 @@
 var http = require('http');
+var url = require('url');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
+
+  var q = url.parse(req.url, true).query;
+  var txt = q.year + " " + q.month;
+  res.write(txt);
+  // http://localhost:8080/?year=2017&month=July
   res.end('Hello World with header');
 }).listen(8080);
