35f9f2569ea68e393cc74f51128bf1d56a2c8269
hiwafa <mq.func@gmail.com>
2024-04-25 15:42:51 +0200

    writing new data

--- nodejs-demo005.js
+++ nodejs-demo005.js
@@ -9,8 +9,13 @@ var fs = require('fs');
 
 
 
-fs.open('nodejs-demo003.html', 'w', function (err, file) {
-  if (err) throw err;
-  console.log(JSON.stringify(file))
-  console.log('Saved!');
+// fs.open('nodejs-demo003.html', 'w', function (err, file) {
+//   if (err) throw err;
+//   console.log(JSON.stringify(file))
+//   console.log('Saved!');
+// });
+
+fs.writeFile('nodejs-demo003.html', 'Hi dear, how are you?', function(err) {
+    if(err) throw err;
+    console.log('saved..')
 });
