d6ac71b0f9e24c4851d6250f483273dc1af79c5f
hiwafa <mq.func@gmail.com>
2024-04-25 15:39:25 +0200

    appending new data

--- nodejs-demo005.js
+++ nodejs-demo005.js
@@ -1,7 +1,16 @@
 
 var fs = require('fs');
 
-fs.appendFile('nodejs-demo003.html', 'Hello content!', function (err) {
-    if (err) throw err;
-    console.log('Saved!');
+// fs.appendFile('nodejs-demo003.html', 'Hello content!', function (err) {
+//     if (err) throw err;
+//     console.log('Saved!');
+// });
+
+
+
+
+fs.open('nodejs-demo003.html', 'w', function (err, file) {
+  if (err) throw err;
+  console.log(JSON.stringify(file))
+  console.log('Saved!');
 });
